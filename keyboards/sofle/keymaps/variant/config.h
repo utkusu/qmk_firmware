117667e9c65011dff9780262fbc0467e97ae44ca
Utku Suleymanoglu <utkusu@users.noreply.github.com>
2023-11-04 16:27:09 -0400

    add retrotapping and explicit tapping term

--- keyboards/sofle/keymaps/variant/config.h
+++ keyboards/sofle/keymaps/variant/config.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define TAPPING_TERM 200
+#define RETRO_TAPPING
+
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
